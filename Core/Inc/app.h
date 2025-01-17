60edf270f954eb947641a94ec0af858fe9938578
wjdals1022 <rlawjdals011022@naver.com>
2025-01-17 14:55:18 +0900

    header file upload

---
+++ Core/Inc/app.h
@@ -0,0 +1,19 @@
+/*
+ * uart.h
+ *
+ *  Created on: Jan 16, 2025
+ *      Author: rlawj
+ */
+
+#ifndef INC_APP_H_
+#define INC_APP_H_
+
+#include "main.h"
+
+
+// function prototype
+
+void app();
+void SystickCallback();
+
+#endif /* INC_APP_H_ */
